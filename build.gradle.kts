507215e721735e911a8c1c8b247356c01a1d2f83
RSC Schmidiger <raoul.schmidiger@gmail.com>
2024-12-16 01:32:07 +0800

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,22 @@
-import org.gradle.kotlin.dsl.`kotlin-dsl`
-
 plugins {
     `java-gradle-plugin`
     `maven-publish`
-//    id("com.gradle.build-scan") version "2.1"
-//    id("java-gradle-plugin")
-//    id("com.github.hierynomus.license") version "0.15.0"
-//    id("com.gradle.plugin-publish") version "0.12.0"
-//    id("com.github.ben-manes.versions") version "0.20.0"
-//    id("maven-publish")
+    id("com.gradle.plugin-publish") version "1.2.1" // Plugin Publishing Plugin
 }
-
-//buildScan {
-//    termsOfServiceUrl   = "https://gradle.com/terms-of-service"
-//    termsOfServiceAgree = "yes"
-//}
-
 
 repositories {
     mavenCentral()
-    maven(
-        "https://plugins.gradle.org/m2/"
-    )
+    gradlePluginPortal() // Recommended for plugin publishing
 }
 
 dependencies {
-//    implementation gradleApi()
-
     implementation("com.google.gradle:osdetector-gradle-plugin:1.7.0")
     implementation("org.javamodularity:moduleplugin:1.8.12")
-
-//    testImplementation gradleTestKit()
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
 
@@ -48,54 +28,39 @@ gradlePlugin {
     plugins {
         create("javafxPlugin") {
             id = "com.github.javafx-gradle-plugin"
-            displayName = "JavaFX Gradle Plugin - raoulsson-tweak"
-            description = "Plugin that makes it easy to work with JavaFX"
+            displayName = "JavaFX Gradle Plugin"
+            description = "A Gradle plugin that simplifies working with JavaFX projects."
             implementationClass = "org.openjfx.gradle.JavaFXPlugin"
         }
     }
 }
-
-//pluginBundle {
-//    website = "https://github.com/openjfx/javafx-gradle-plugin"
-//    vcsUrl = "https://github.com/openjfx/javafx-gradle-plugin"
-//    tags = listOf( "java", "javafx" )
-//}
 
 group = "com.github.raoulsson"
 version = "0.0.17-raoulsson"
 
 publishing {
     publications {
-        create<MavenPublication>("maven"){
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
             groupId = "com.github.raoulsson"
             artifactId = "javafx-gradle-plugin"
             version = "0.0.17-raoulsson"
-
-            from(components["java"])
         }
     }
     repositories {
         maven {
-            // change to point to your repo, e.g. http://my.org/repo
-            url = uri(layout.buildDirectory.dir("repo"))
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/raoulsson/javafx-gradle-plugin")
+            credentials {
+                username = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
         }
     }
-//    repositories {
-//        maven {
-//            if (project.hasProperty("sonatypeUsername") && project.hasProperty("sonatypePassword")) {
-//                credentials {
-//                    username project.property("sonatypeUsername")
-//                    password project.property("sonatypePassword")
-//                }
-//                url = "https://oss.sonatype.org/content/repositories/snapshots/"
-//            }
-//        }
-//    }
 }
 
-//license {
-//    skipExistingHeaders = true
-//    mapping {
-//        java = "SLASHSTAR_STYLE"
-//    }
-//}
+pluginBundle {
+    website = "https://github.com/openjfx/javafx-gradle-plugin"
+    vcsUrl = "https://github.com/openjfx/javafx-gradle-plugin"
+    tags = listOf("java", "javafx", "gradle-plugin")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal() // Required for retrieving the com.gradle.plugin-publish plugin
+        mavenCentral()       // Additional repository for Maven artifacts
+    }
+}
+
+rootProject.name = "javafx-plugin"
